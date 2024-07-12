3c9bf0e6c357ee8437533ad7c2dabdf1d63dff51
Yoann Moise <myoann@users.noreply.github.com>
2024-07-12 11:38:42 +0200

    feat(packages): Update packages and tests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 
 import Header from "@/components/Header";
 
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Plan my trip",
   description: "A web app that gives you basic data on a few places",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type TProps = Readonly<{
   children: React.ReactNode;
-}>) {
+}>;
+
+export default function RootLayout({ children }: TProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
       </body>
